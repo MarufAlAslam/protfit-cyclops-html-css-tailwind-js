db7718549c3f8cdebaa246d01af56ef66aebfdea
marufalaslam <maruf14@cse.pstu.ac.bd>
2023-10-24 08:15:44 +0600

    table done

--- js/index.js
+++ js/index.js
@@ -2,3 +2,8 @@ const dateRangePickerEl = document.getElementById("dateRangePickerId");
 new DateRangePicker(dateRangePickerEl, {
   // options
 });
+
+const refuntCostBtn = document.getElementById("refund-cost");
+refuntCostBtn.addEventListener("click", () => {
+  alert("Refund cost");
+});
