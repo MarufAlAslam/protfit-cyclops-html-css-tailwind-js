07cea5a3666337b1eb5fbfaa91c359c5ae76a462
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-10-25 10:24:05 +0600

    revision-8: toggle color change

--- js/index.js
+++ js/index.js
@@ -4,47 +4,62 @@
 // });
 
 const refuntCostBtn = document.getElementById("refund-cost");
+const refundCostTr = document.getElementById("refund-cost-tr");
 refuntCostBtn.addEventListener("click", () => {
-  //   by clicking on it, show all of it's sibling with class toggleable-tr
+  refundCostTr.classList.toggle("bg-gray-100")
   const toggleableTrs = document.querySelectorAll(".refund-tr");
   toggleableTrs.forEach((tr) => {
     tr.classList.toggle("show");
+    tr.classList.toggle("bg-gray-100");
   });
 });
 
 const fulfilment = document.getElementById("fulfilment");
+const fulfilmentTr = document.getElementById("fulfilment-tr");
 fulfilment.addEventListener("click", () => {
-  //   by clicking on it, show all of it's sibling with class toggleable-tr
+  fulfilmentTr.classList.toggle("bg-gray-100")
   const fulfilmentTrs = document.querySelectorAll(".fulfilment-tr");
   fulfilmentTrs.forEach((tr) => {
     tr.classList.toggle("show");
+    tr.classList.toggle("bg-gray-100");
   });
 });
 
 const advertFees = document.getElementById("advert-fees");
+const advertTr = document.getElementById("advert-tr");
 advertFees.addEventListener("click", () => {
-  //   by clicking on it, show all of it's sibling with class toggleable-tr
+  advertTr.classList.toggle("bg-gray-100")
   const advertTrs = document.querySelectorAll(".advert-tr");
+  const adCostTr = document.querySelectorAll(".ad-cost-tr");
   advertTrs.forEach((tr) => {
     tr.classList.toggle("show");
+    tr.classList.toggle("bg-gray-100");
+  });
+  adCostTr.forEach((tr) => {
+    tr.classList.remove("show");
+    tr.classList.toggle("bg-gray-100");
   });
 });
 
 const adsCost = document.getElementById("ads-cost");
+const adsCostTr = document.getElementById("ads-cost-tr");
 adsCost.addEventListener("click", () => {
-  //   by clicking on it, show all of it's sibling with class toggleable-tr
+  adsCostTr.classList.toggle("bg-gray-200")
   const adCostTr = document.querySelectorAll(".ad-cost-tr");
   adCostTr.forEach((tr) => {
     tr.classList.toggle("show");
+    tr.classList.toggle("bg-gray-200");
   });
 });
 
 const inventoryFees = document.getElementById("inventory-fees");
+const inventoryTrr = document.getElementById("inventory-tr");
 inventoryFees.addEventListener("click", () => {
-  //   by clicking on it, show all of it's sibling with class toggleable-tr
+  inventoryTrr.classList.toggle("bg-gray-100")
   const inventoryTr = document.querySelectorAll(".inventory-tr");
   inventoryTr.forEach((tr) => {
     tr.classList.toggle("show");
+    tr.classList.toggle("bg-gray-100");
   });
 });
 
