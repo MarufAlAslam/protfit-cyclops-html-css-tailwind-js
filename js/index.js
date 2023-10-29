536ee2c80bee02a9ec8305bb54e6b789ae895e57
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-10-29 09:47:16 +0600

    icons functional

--- js/index.js
+++ js/index.js
@@ -6,38 +6,38 @@
 const refuntCostBtn = document.getElementById("refund-cost");
 const refundCostTr = document.getElementById("refund-cost-tr");
 refuntCostBtn.addEventListener("click", () => {
-  refundCostTr.classList.toggle("bg-gray-100")
+  refundCostTr.classList.toggle("active-bg")
   const toggleableTrs = document.querySelectorAll(".refund-tr");
   toggleableTrs.forEach((tr) => {
     tr.classList.toggle("show");
-    tr.classList.toggle("bg-gray-100");
+    tr.classList.toggle("active-bg");
   });
 });
 
 const fulfilment = document.getElementById("fulfilment");
 const fulfilmentTr = document.getElementById("fulfilment-tr");
 fulfilment.addEventListener("click", () => {
-  fulfilmentTr.classList.toggle("bg-gray-100")
+  fulfilmentTr.classList.toggle("active-bg")
   const fulfilmentTrs = document.querySelectorAll(".fulfilment-tr");
   fulfilmentTrs.forEach((tr) => {
     tr.classList.toggle("show");
-    tr.classList.toggle("bg-gray-100");
+    tr.classList.toggle("active-bg");
   });
 });
 
 const advertFees = document.getElementById("advert-fees");
 const advertTr = document.getElementById("advert-tr");
 advertFees.addEventListener("click", () => {
-  advertTr.classList.toggle("bg-gray-100")
+  advertTr.classList.toggle("active-bg")
   const advertTrs = document.querySelectorAll(".advert-tr");
   const adCostTr = document.querySelectorAll(".ad-cost-tr");
   advertTrs.forEach((tr) => {
     tr.classList.toggle("show");
-    tr.classList.toggle("bg-gray-100");
+    tr.classList.toggle("active-bg");
   });
   adCostTr.forEach((tr) => {
     tr.classList.remove("show");
-    tr.classList.toggle("bg-gray-100");
+    tr.classList.toggle("active-bg");
   });
 });
 
@@ -55,11 +55,21 @@ adsCost.addEventListener("click", () => {
 const inventoryFees = document.getElementById("inventory-fees");
 const inventoryTrr = document.getElementById("inventory-tr");
 inventoryFees.addEventListener("click", () => {
-  inventoryTrr.classList.toggle("bg-gray-100")
+  inventoryTrr.classList.toggle("active-bg")
   const inventoryTr = document.querySelectorAll(".inventory-tr");
   inventoryTr.forEach((tr) => {
     tr.classList.toggle("show");
-    tr.classList.toggle("bg-gray-100");
+    tr.classList.toggle("active-bg");
+  });
+});
+const otherFees = document.getElementById("other-fees");
+const otherTrr = document.getElementById("other-tr");
+otherFees.addEventListener("click", () => {
+  otherTrr.classList.toggle("active-bg")
+  const otherTr = document.querySelectorAll(".other-tr");
+  otherTr.forEach((tr) => {
+    tr.classList.toggle("show");
+    tr.classList.toggle("active-bg");
   });
 });
 
@@ -74,3 +84,48 @@ sidebarToggler.addEventListener("click", () => {
 sidebarBg.addEventListener("click", () => {
   sidebar.classList.remove("show");
 });
+
+
+const datepicker = document.getElementsByClassName('datepicker')
+
+// datepicker will be hidden once user starts scrolling
+window.addEventListener('scroll', function () {
+  for (let i = 0; i < datepicker.length; i++) {
+    datepicker[i].classList.add('hidden')
+  }
+})
+
+const dropdownHelperRadioButton = document.getElementById('dropdownHelperRadioButton')
+const dropdownHelperRadioButtonx = document.getElementById('dropdownHelperRadioButtonx')
+const dropdownHelperRadioButtony = document.getElementById('dropdownHelperRadioButtony')
+const overlay = document.getElementById('overlay')
+
+// clicking on dropdownHelperRadioButton, the icon inside will change direction
+dropdownHelperRadioButton.addEventListener('click', function () {
+  const dropdownHelperIcon = document.getElementById('dropdownHelperIcon')
+  dropdownHelperIcon.classList.toggle('rotate-180')
+  overlay.classList.toggle('show')
+})
+// clicking on dropdownHelperRadioButton, the icon inside will change direction
+dropdownHelperRadioButtonx.addEventListener('click', function () {
+  const dropdownHelperIcon = document.getElementById('dropdownHelperIconx')
+  dropdownHelperIcon.classList.toggle('rotate-180')
+  overlay.classList.toggle('show')
+})
+// clicking on dropdownHelperRadioButton, the icon inside will change direction
+dropdownHelperRadioButtony.addEventListener('click', function () {
+  const dropdownHelperIcon = document.getElementById('dropdownHelperIcony')
+  dropdownHelperIcon.classList.toggle('rotate-180')
+  overlay.classList.toggle('show')
+})
+
+// clicking on overlay, the icon inside will change direction
+overlay.addEventListener('click', function () {
+  const dropdownHelperIcon = document.getElementById('dropdownHelperIcon')
+  const dropdownHelperIconx = document.getElementById('dropdownHelperIconx')
+  const dropdownHelperIcony = document.getElementById('dropdownHelperIcony')
+  dropdownHelperIcon.classList.toggle('rotate-180')
+  dropdownHelperIconx.classList.toggle('rotate-180')
+  dropdownHelperIcony.classList.toggle('rotate-180')
+  overlay.classList.toggle('show')
+})
